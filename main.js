f041898e4ca5a2d296ab249b1699636659f51102
WaysideTester <dylanmurray2119@gmail.com>
2018-09-22 12:22:25 -0500

    encoding/decoding function

--- main.js
+++ main.js
@@ -2,4 +2,29 @@ const printToDom = (divId, stringToPrint) => {
     document.getElementById(divId).innerHTML = stringToPrint;
 };
 
-printToDom('print-here', 'i work');
+const testinput = 'asdf';
+let encoded = [];
+let decoded = [];
+
+const utf16Encoder = (input) => {
+    let newArray = input.split('');
+    for (i = 0; i < newArray.length; i++){
+        let charCode = input.charCodeAt(i);
+        console.log(charCode);
+        encoded.push(charCode);
+    }
+};
+
+const uft61Decoder = (input) => {
+    let newArray = input
+    for (i = 0; i < newArray.length; i++){
+        let char = String.fromCharCode(input[i]);
+        console.log(char);
+        decoded.push(char);
+    }
+};
+
+utf16Encoder(testinput);
+console.log(encoded);
+uft61Decoder(encoded);
+console.log(decoded);
